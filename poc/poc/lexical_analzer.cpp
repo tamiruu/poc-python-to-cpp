92e27d81aaf80e06b83e71df113d843996c9596c
tamiruu <tamir14t@gmail.com>
2022-08-23 15:59:01 +0300

    lexical analizer wip

--- poc/poc/lexical_analzer.cpp
+++ poc/poc/lexical_analzer.cpp
@@ -33,3 +33,33 @@ void clean_comments(std::vector<std::string>& code) {
         *i = content;
     }
 } 
+
+token::token(std::string value, std::string type) {
+    this->token_value = value;
+    this->type = type;
+}
+
+std::vector<token> tokenizer(std::vector<std::string>& code) {
+    std::vector<token> tokens;
+    clean_comments(code);
+    
+    clean_tab_and_space(code);
+    for (std::vector<std::string>::iterator i = code.begin(); i != code.end(); ++i)
+    {
+        for (int j = 0; j < i->size(); j++)
+        {
+            if (is_keywords(i[j]))
+            {
+                token tok(i[j], "keyword");
+            }
+            else if (is_operator(i[j]))
+            {
+                token tok(i[j], "operator");
+            }
+            else if ((i[j]))
+            {
+
+            }
+        }
+    }
+}
